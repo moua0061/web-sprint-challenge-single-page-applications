e1945e719588830fe2e619bb8e29e01f3e0cb0bd
Lia Moua <moua0061@yahoo.com>
2021-10-15 06:01:20 -0400

    trying to get the route and link to work

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import pizza from './Pizza.jpg';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
-
+//component folders being imported
+import Form from './Components.js/Form';
 
 
 
@@ -15,13 +17,31 @@ const App = () => {
   return (
     <div className='header'>
       <h1>Lambda Eats</h1>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+
+        <li>
+          <Link to='/help'>Help</Link>
+        </li>
+
+        <li>
+          <Link to='/Form'>Order</Link>
+        </li>
+      </ul> 
+
       <div className='img-logo-container'>
         <img src={pizza} alt='pizza logo' />
       </div>
 
       <div className='main-container'>
         <div className='order-button'>
-          <button id='orderBtn'>Order Now!</button>
+
+          <Link to='/Form'>
+            <button id='orderBtn'>Order Now!</button>
+          </Link>
+          
         </div>
           
       </div>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import Form from './Components.js/Form';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <Router>
+        <Switch>
+            <App />
+        </Switch>
+    </Router>
+, document.getElementById("root"));
