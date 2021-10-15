214a49d4680c1b2422d13cc25527ea124a7e593a
Lia Moua <moua0061@yahoo.com>
2021-10-15 05:24:53 -0400

    added pizza logo and button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from "react";
+import pizza from './Pizza.jpg';
+import styled from 'styled-components';
+
+
+
+
+
+
+
+
+
 
 const App = () => {
   return (
-    <>
+    <div className='header'>
       <h1>Lambda Eats</h1>
-      <p>Your Favorite Food, Delivered While Coding</p>
-    </>
+      <div className='img-logo-container'>
+        <img src={pizza} alt='pizza logo' />
+      </div>
+
+      <div className='main-container'>
+        <div className='order-button'>
+          <button id='orderBtn'>Order Now!</button>
+        </div>
+          
+      </div>
+        <p>Food Delivery in Gotham City</p>
+    </div>
+    
   );
 };
 export default App;
